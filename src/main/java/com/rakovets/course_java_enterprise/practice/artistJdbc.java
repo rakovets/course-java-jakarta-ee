0193b8400cdb513101ab4d5683e91807485a5588
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-06-02 20:18:44 +0300

    did second task

--- src/main/java/com/rakovets/course_java_enterprise/practice/artistJdbc.java
+++ src/main/java/com/rakovets/course_java_enterprise/practice/artistJdbc.java
@@ -10,6 +10,7 @@ class artistJdbc {
     private static final String DATASOURCE_PASSWORD = "12340987";
 
     public static void main(String[] args) {
+        Boolean menuWork = true;
         Scanner scanner = new Scanner(System.in);
         try{
             Class.forName(DATASOURCE_DRIVER);
@@ -19,24 +20,41 @@ class artistJdbc {
         }
         try(Connection connection = DriverManager.getConnection(DATASOURCE_URL, DATASOURCE_USER, DATASOURCE_PASSWORD)) {
             Statement statement = connection.createStatement();
-            String answer = scanner.nextLine();
-            menu();
-            if (answer.equals("1")) {
-                getArtists(statement);
+
+            while (menuWork) {
+                menu();
+                String answer = scanner.nextLine();
+                if (answer.equals("1")) {
+                    getArtists(statement);
+                } else if (answer.equals("2")) {
+                    getSongs(statement);
+                }
             }
+
         } catch (SQLException e) {
             System.out.println("SQLException: " + e);
         }
     }
 
     public static void menu() {
-        System.out.println("Choose:\n1. Get all artists\n");
+        System.out.println("\nChoose number:\n1. Get all artists\n2. Get all songs\n");
     }
 
     public static void getArtists(Statement statement) throws SQLException {
         ResultSet rs = statement.executeQuery("SELECT * FROM artist");
+        System.out.println("Artists:\nid" + "\t" +"name");
         while (rs.next()) {
             System.out.println(rs.getInt(1) + "\t" + rs.getString(2));
+        }
+    }
+
+    public static void getSongs(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT * FROM song");
+        System.out.println("Songs:\nid, title, length, artist_id\n" +
+                "-----------------------------------");
+        while (rs.next()) {
+            System.out.println(rs.getInt("id") + ", " + rs.getString("title") +
+                    ", " + rs.getInt("length") + ", " + rs.getInt("artist_id"));
         }
     }
 }
