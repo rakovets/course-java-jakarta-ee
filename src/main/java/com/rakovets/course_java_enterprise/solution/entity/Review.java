630c26ed33f2cb1e23ccd5f0a9e75ca17036cfa9
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-06-06 14:40:30 +0300

    create entity Review with methods

--- src/main/java/com/rakovets/course_java_enterprise/solution/entity/Review.java
+++ src/main/java/com/rakovets/course_java_enterprise/solution/entity/Review.java
@@ -1,4 +1,45 @@
 package main.java.com.rakovets.course_java_enterprise.solution.entity;
 
 public class Review {
+    private long id;
+    private String content;
+    private Restaurant restaurant;
+
+    public Review(String content, Restaurant restaurant) {
+        this.content = content;
+        this.restaurant = restaurant;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", restaurant=" + restaurant +
+                '}';
+    }
 }
