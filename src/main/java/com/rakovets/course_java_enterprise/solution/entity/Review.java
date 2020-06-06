91b7498e83528daed4b9f53c7552ed1a33dd911b
AlexeyAleks <AlexeyAleks@mail.ru>
2020-06-06 21:59:37 +0300

    solution Tasks01-03

---
+++ src/main/java/com/rakovets/course_java_enterprise/solution/entity/Review.java
@@ -0,0 +1,41 @@
+package main.java.com.rakovets.course_java_enterprise.solution.entity;
+
+public class Review {
+	private int id;
+	private String content;
+	private int restaurantID;
+
+	public Review(String content, int restaurantID) {
+		this.content = content;
+		this.restaurantID = restaurantID;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getRestaurantID() {
+		return restaurantID;
+	}
+
+	public void setRestaurantID(int restaurantID) {
+		this.restaurantID = restaurantID;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Review { id = %-3d, content = '%s', restaurantID = %d }", id, content, restaurantID);
+	}
+}
