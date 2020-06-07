5ed71621566288beba095d4bc32218ec9b771826
AlexeyAleks <AlexeyAleks@mail.ru>
2020-06-07 11:36:13 +0300

    solution Tasks01-06

--- src/main/java/com/rakovets/course_java_enterprise/solution/entity/Restaurant.java
+++ src/main/java/com/rakovets/course_java_enterprise/solution/entity/Restaurant.java
@@ -1,8 +1,11 @@
 package main.java.com.rakovets.course_java_enterprise.solution.entity;
 
+import java.util.List;
+
 public class Restaurant {
 	private int id;
 	private String name;
+	private List<Dish> listDish;
 
 	public Restaurant(String name) {
 		this.name = name;
@@ -22,6 +25,14 @@ public class Restaurant {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Dish> getListDish() {
+		return listDish;
+	}
+
+	public void setListDish(List<Dish> listDish) {
+		this.listDish = listDish;
 	}
 
 	@Override
