4c3b3dee04dec233a1521c9f44d629cb8a4b3260
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-06-07 21:10:03 +0300

    env: build web dynamic applications

--- .deploy/build.sh
+++ .deploy/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo "Build *.war for web-application"
 jar -cvf web-static-app.war -C web-static-app .
+find . -name "*.java" -print | xargs javac -cp web-dynamic-app/WEB-INF/lib/javax.servlet-api-4.0.1.jar
+jar -cvf web-dynamic-app.war -C web-dynamic-app .
+jar -cvf web-dynamic-modern-app.war -C web-dynamic-modern-app .
