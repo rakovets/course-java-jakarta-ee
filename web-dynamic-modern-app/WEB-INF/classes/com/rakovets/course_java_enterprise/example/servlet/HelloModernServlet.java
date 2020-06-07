dc4311e9b79d6d3c88b48634ed1ad39900184fdc
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-06-07 21:09:07 +0300

    feat: web application with dynamic content (servlet and annotation)

---
+++ web-dynamic-modern-app/WEB-INF/classes/com/rakovets/course_java_enterprise/example/servlet/HelloModernServlet.java
@@ -0,0 +1,20 @@
+package com.rakovets.course_java_enterprise.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloModernServlet", urlPatterns = {"/sayhello"})
+public class HelloModernServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<p><span style='color: Red;'>Hi, world!</span></p>");
+    }
+}
