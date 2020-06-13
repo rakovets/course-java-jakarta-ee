3dd46c38a19cd49bd5be16e4039e511d0eb20a56
Nikitatrip228 <nikitaatesh@icloud.com>
2020-06-13 21:28:22 +0300

    Add classes

---
+++ src/main/java/com/rakovets/course_java_enterprise/solution/DemoGet.java
@@ -0,0 +1,4 @@
+package main.java.com.rakovets.course_java_enterprise.solution;
+
+public class DemoGet {
+}
---
+++ src/main/java/com/rakovets/course_java_enterprise/solution/SendNameAndSurname.java
@@ -0,0 +1,5 @@
+package main.java.com.rakovets.course_java_enterprise.solution;
+
+public class SendNameAndSurname {
+    
+}
