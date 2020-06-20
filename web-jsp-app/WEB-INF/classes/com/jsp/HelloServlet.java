9b80efc15004b263f91f82815138a790434eca9f
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-06-20 11:54:21 +0300

    Task 2: add servlet HelloServlet with request dispatcher on jsp

---
+++ web-jsp-app/WEB-INF/classes/com/jsp/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.jsp;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = "/hello.jsp";
+        req.getRequestDispatcher(path).forward(req, resp);
+    }
+}
