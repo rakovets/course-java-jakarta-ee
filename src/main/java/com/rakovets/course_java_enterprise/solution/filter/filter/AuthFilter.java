2081e479a6be41d001a8bf60ad0a39656fd62559
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-06-25 22:10:15 +0300

    draft

---
+++ src/main/java/com/rakovets/course_java_enterprise/solution/filter/filter/AuthFilter.java
@@ -0,0 +1,30 @@
+package com.rakovets.course_java_enterprise.solution.filter.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/orders", "/forward", "/redirect"})
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String contextPath = req.getContextPath();
+        HttpSession session = req.getSession();
+        if ((session != null) && (session.getAttribute("user") != null)) {
+            chain.doFilter(request, response);
+        } else {
+            res.sendRedirect(contextPath + "/login.jsp");
+        }
+    }
+}
+
