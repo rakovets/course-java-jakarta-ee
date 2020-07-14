995f4b6f9f71b1b522f424803374c91bf60a918f
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-07-14 13:14:41 +0300

    done tasks with calculator by apach maven and connect jacoco

---
+++ src/main/java/com/rakovets/course_java_enterprise/solution/Calculator.java
@@ -0,0 +1,19 @@
+package com.rakovets.course_java_enterprise.solution;
+
+public class Calculator {
+    public double add(double firstValue, double secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public double subtract(double firstValue,double secondValue) {
+        return firstValue - secondValue;
+    }
+
+    public double multiply(double firstValue,double secondValue) {
+        return firstValue * secondValue;
+    }
+
+    public double divide(double firstValue,double secondValue) {
+        return firstValue / secondValue;
+    }
+}
---
+++ src/test/java/com/trutsin/apache_maven/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.trutsin.apache_maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(12, calculator.add(2, 10));
+        assertEquals(3.25, calculator.add(-2.3, 5.55));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(14, calculator.subtract(20, 6));
+        assertEquals(12.5, calculator.subtract(14, 1.5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(12.5, calculator.multiply(5, 2.5));
+        assertEquals(13.2, calculator.multiply(3.3, 4));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(36.25, calculator.divide(145, 4));
+        assertEquals(2.8, calculator.divide(14, 5));
+    }
+}
