abb254817e10485b686128641c28cff4b7096167
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-07-14 16:57:54 +0300

    Tasks 1-6: add calculator and test for it, create profile with plugins in pom.xml and test it

---
+++ src/main/java/com/rakovets/course_java_enterprise/solution/Calculator.java
@@ -0,0 +1,19 @@
+package com.rakovets.course_java_enterprise.solution;
+
+public class Calculator {
+    public double add(final double firstValue, final double secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public double subtract(final double firstValue, final double secondValue) {
+        return firstValue - secondValue;
+    }
+
+    public double multiply(final double firstValue, final double secondValue) {
+        return firstValue * secondValue;
+    }
+
+    public double divide(final double firstValue, final double secondValue) {
+        return firstValue / secondValue;
+    }
+}
