6e651fa89c2cf980de83e7aaa940a09531af73e7
Dmitry Rakovets <dmitryrakovets@gmail.com>
2021-12-15 17:49:43 +0300

    feat: add example for theme 'Jakarta EE: Servlet'

---
+++ 03-web-servlet-app/src/main/java/by/rakovets/course/java/jakarta_ee/servlet/example/HttpPathParametersReaderServlet.java
@@ -0,0 +1,27 @@
+package by.rakovets.course.java.jakarta_ee.servlet.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/api/users/*")
+public class HttpPathParametersReaderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
+        String bodyAsJson = "{\n" +
+                "\t\"pathInfo\":\"" + pathInfo + "\"\n" +
+                "}";
+
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
+        writer.write(bodyAsJson);
+    }
+}
